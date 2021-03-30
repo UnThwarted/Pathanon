153117bf2c3405f2abf63311a0ebcb7161404939
Phil Reid <phil_reid_uk@hotmail.com>
2021-03-30 09:48:46 +0100

    Add project files.

---
+++ PathAnon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathAnon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parameters
+            // FilePath - path of xml file to be anonymised
+            // xpath location of Tag or Attribute to be replaced
+            // path of text file containing your replacement text (optional)
+            Console.WriteLine("Under construction");
+        }
+    }
+}
